6fec768a3227310db947d2a9211a4a87679b2efc
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-06-21 00:51:36 +0600

    inplement APi for abailable food

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
@@ -5,8 +6,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bejl412.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,18 +26,26 @@ async function run() {
         const database = client.db("foodDB");
         const foodCollection = database.collection("foods");
 
-       //creat a new food item
-        app.post('/foods', async (req, res) => {
+        //creat a new food item
+        app.post('/add-food', async (req, res) => {
             const food = req.body;
             const result = await foodCollection.insertOne(food);
             res.send(result);
-        })
+        });
+
         //get all food items
-            app.get('/foods', async (req, res) => {
+        app.get('/foods', async (req, res) => {
             const foods = await foodCollection.find().toArray();
             res.send(foods);
-           // console.log(foods);
-        })
+            // console.log(foods);
+        });
+
+        //get only available food items
+        app.get('/available-foods', async (req, res) => {
+            const availableFoods = await foodCollection.find({ status: "available" }).toArray();
+            res.send(availableFoods);
+        });
+
         // Send a ping to confirm a successful connection
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
