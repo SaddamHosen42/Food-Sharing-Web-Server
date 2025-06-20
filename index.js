c77fb5db13fc7fbe038df71c6d261b711b9f7377
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-06-20 12:03:20 +0600

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 5000;
-
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bejl412.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    // Send a ping to confirm a successful connection
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
