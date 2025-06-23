141b5499bf88918c5e4f58038b9af531c4bad202
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-06-23 23:11:25 +0600

    add jwt

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
+
+const admin = require("firebase-admin");
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -20,6 +22,29 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+
+const serviceAccount = require("./firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const verifyJWT =async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader||!authHeader.startsWith('Bearer ')) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = await admin.auth().verifyIdToken(token);
+        req.decoded = decoded;
+        next();
+    } catch (error) {
+        res.status(401).send({ message: 'Unauthorized access' });
+    }
+}
+
 async function run() {
     try {
 
@@ -34,10 +59,13 @@ async function run() {
         });
 
         //get all food items and get added by user
-        app.get('/foods', async (req, res) => {
+        app.get('/foods',verifyJWT, async (req, res) => {
             const email = req.query.email;
-            //console.log(`Querying foods for email: ${email}`);
-
+           
+            if(email!== req.decoded.email){
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+            
             const query = {};
             if (email) {
                 query.donorEmail = email;
@@ -72,7 +100,7 @@ async function run() {
             const availableFoods = await foodCollection.find({ status: "available" }).toArray();
             res.send(availableFoods);
         });
-        //get only 6 available food items
+        //get only 6 featured Foods represent the highest quantity of food options available.
         app.get('/featured-foods', async (req, res) => {
 
             const featuredFoods = await foodCollection
